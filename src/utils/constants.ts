deb74f64c3873517c2ae385213be301c0666b9e7
Daniel Andeche <danielandeche720@gmail.com>
2026-03-22 04:02:48 +0300

    Update PRODUCTION_APP_ID and add DEFAULT_API_DOMAIN

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const PRODUCTION_APP_ID = '36544';
+export const PRODUCTION_APP_ID = '84769';
 export const STAGING_APP_ID = '36545';
 export const VERCEL_DEPLOYMENT_APP_ID = '35073';
 export const LOCALHOST_APP_ID = '35074';
@@ -7,6 +7,7 @@ export const PRODUCTION_BE_APP_ID = '61570';
 export const STAGING_ME_APP_ID = '61571';
 export const STAGING_BE_APP_ID = '61572';
 
+export const DEFAULT_API_DOMAIN = 'https://api.derivmarkup.site/';
 export const DEFAULT_WS_SERVER = 'ws.derivws.com';
 export const OAUTH_URL = 'oauth.deriv.com';
 
